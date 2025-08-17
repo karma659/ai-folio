179c89c0d36ff337127c83802c6d3a3bb5958681
karma659 <55893200+karma659@users.noreply.github.com>
2025-08-17 04:52:24 +0000

    Deploying to gh-pages from @ karma659/ai-folio@fcfbf2226f96a51c3262ea68ff8ede969cab73ee 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -416,9 +416,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-worldquant-international-quant-championship",
+          title: 'WorldQuant International Quant Championship',
+          description: "Creating alpha strategies for portfolio construction",
           section: "Projects",handler: () => {
               window.location.href = "/ai-folio/projects/1_project/";
             },},{id: "projects-project-2",
