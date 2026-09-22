f7b4005c7e6b06e725251be7d2f7c67c0e372271
karma659 <55893200+karma659@users.noreply.github.com>
2026-09-22 04:16:32 +0000

    Deploying to gh-pages from @ karma659/ai-folio@c1a68011008c94def75c2a60eb2b83e6f163d7fb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -67,30 +67,4 @@ ninja.data = [{
           description: "Zicklin Case Competition (conceptualize an AI startup)",
           section: "Projects",handler: () => {
               window.location.href = "/ai-folio/projects/zicklin/";
-            },},{
-      id: 'light-theme',
-      title: 'Change theme to light',
-      description: 'Change the theme of the site to Light',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("light");
-      },
-    },
-    {
-      id: 'dark-theme',
-      title: 'Change theme to dark',
-      description: 'Change the theme of the site to Dark',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("dark");
-      },
-    },
-    {
-      id: 'system-theme',
-      title: 'Use system default theme',
-      description: 'Change the theme of the site to System Default',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("system");
-      },
-    },];
+            },},];
